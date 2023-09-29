17ef5ffab9364537a595591045e307a1f6763b3b
user4 <user4@DESKTOP-4VA2173>
2023-09-29 14:49:35 +0530

    commit demo file

---
+++ devops1/src/devops1/demo.java
@@ -0,0 +1,12 @@
+package devops1;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world!");
+		System.out.println("this is java project");
+
+	}
+
+}
